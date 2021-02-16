cc37fb8fc3777f4f92be69c90672fd27908dc59e
AKIMANA Rachel <rachelakimana@gmail.com>
2021-02-16 18:45:27 +0200

    Add test nav-link

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,15 @@ const Header = () => {
     <div className=" h-screen w-screen bg-white">
       <div className="fixed bg-red-700 h-12 w-full flex justify-center items-center text-xl text-white">
         <div className="pl-12 capitalize hover:text-black cursor-pointer">
+          <button
+            type="button"
+            onClick={() => setClicked('Test')}
+            className={isClicked === 'Test' ? 'capitalize text-blue-700' : 'capitalize'}
+          >
+            Test
+          </button>
+        </div>
+        <div className="pl-12 capitalize hover:text-black cursor-pointer">
           <Headercolor isActive={isClicked === 'home'} setClicked={setClicked}>
             home
           </Headercolor>
